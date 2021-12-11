7d915531137e3c996541a36db4985109ffa17771
nirjo <nirmalraj.joseph86@gmail.com>
2021-12-11 13:43:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'react-bootstrap' ;
+import  Entry  from './page/Entry-page';
+function App() {
+  return (
+    <div className="App">
+     <p>Nirmals CRM - frontend page</p>
+     <Entry />
+     <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Entry-page.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./entry-style.css";
+
+const Entry = ()=>{
+          return(
+                    <div className ="entry-page bg-info">Entry page</div>
+          )
+}
+
+export default Entry;
